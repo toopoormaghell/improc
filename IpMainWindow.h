fa36ed61d7767eba0316beb231de36b217803dc2
Nicolas <nicolas.combaret@gmail.com>
2015-04-08 22:39:47 +0200

    [Enh] Copy one of the example for a basic application
    The application allows loading an image and playing a little with it.

--- IpMainWindow.h
+++ IpMainWindow.h
@@ -4,6 +4,11 @@
 
 
 #include <QMainWindow>
+class QAction;
+class QLabel;
+class QMenu;
+class QScrollArea;
+class QScrollBar;
 
 
 
@@ -19,10 +24,43 @@ class IpMainWindow : public QMainWindow
 
 public:
     explicit IpMainWindow( QWidget *parent = 0 );
-    ~IpMainWindow();
+    bool loadFile( const QString& filename );
+
+private slots:
+    void open();
+    void print();
+    void zoomIn();
+    void zoomOut();
+    void normalSize();
+    void fitToWindow();
+    void about();
 
 private:
+    void createActions();
+    void createMenus();
+    void updateActions();
+    void scaleImage( double factor );
+    void adjustScrollBar( QScrollBar* scrollBar, double factor );
+
     Ui::IpMainWindow *ui;
+
+    QLabel* imageLabel;
+    QScrollArea* scrollArea;
+    double scaleFactor;
+
+    QAction* openAct;
+    QAction* printAct;
+    QAction* exitAct;
+    QAction* zoomInAct;
+    QAction* zoomOutAct;
+    QAction* normalSizeAct;
+    QAction* fitToWindowAct;
+    QAction* aboutAct;
+    QAction* aboutQtAct;
+
+    QMenu* fileMenu;
+    QMenu* viewMenu;
+    QMenu* helpMenu;
 };
 
 
